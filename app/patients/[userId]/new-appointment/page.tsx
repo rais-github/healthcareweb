6d5efe0ed1187c7afc5ed078c62d76b86a176480
Himanshu Rai <himanshu2003rai@gmail.com>
2024-09-12 23:43:05 +0530

    implement patient register and FileUploader

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>New Appointment</div>;
+};
+
+export default page;
---
+++ constants/index.ts
@@ -0,0 +1,88 @@
+export const GenderOptions = ["Male", "Female", "Other"];
+
+export const PatientFormDefaultValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  birthDate: new Date(Date.now()),
+  gender: "Male" as Gender,
+  address: "",
+  occupation: "",
+  emergencyContactName: "",
+  emergencyContactNumber: "",
+  primaryPhysician: "",
+  insuranceProvider: "",
+  insurancePolicyNumber: "",
+  allergies: "",
+  currentMedication: "",
+  familyMedicalHistory: "",
+  pastMedicalHistory: "",
+  identificationType: "Birth Certificate",
+  identificationNumber: "",
+  identificationDocument: [],
+  treatmentConsent: false,
+  disclosureConsent: false,
+  privacyConsent: false,
+};
+
+export const IdentificationTypes = [
+  "Birth Certificate",
+  "Medical Insurance Card/Policy",
+  "Military ID Card",
+  "National Identity Card",
+  "Passport",
+  "Resident Alien Card (Green Card)",
+  "Social Security Card",
+  "State ID Card",
+  "Driver's License",
+  "Student ID Card",
+  "Voter ID Card",
+  "Adhar Card",
+  "Pan Card",
+];
+
+export const Doctors = [
+  {
+    image: "/assets/images/dr-green.png",
+    name: "John Green",
+  },
+  {
+    image: "/assets/images/dr-cameron.png",
+    name: "Leila Cameron",
+  },
+  {
+    image: "/assets/images/dr-livingston.png",
+    name: "David Livingston",
+  },
+  {
+    image: "/assets/images/dr-peter.png",
+    name: "Evan Peter",
+  },
+  {
+    image: "/assets/images/dr-powell.png",
+    name: "Jane Powell",
+  },
+  {
+    image: "/assets/images/dr-remirez.png",
+    name: "Alex Ramirez",
+  },
+  {
+    image: "/assets/images/dr-lee.png",
+    name: "Jasmine Lee",
+  },
+  {
+    image: "/assets/images/dr-cruz.png",
+    name: "Alyana Cruz",
+  },
+  {
+    image: "/assets/images/dr-sharma.png",
+    name: "Himanshu Rai",
+  },
+];
+
+export const StatusIcon = {
+  scheduled: "/assets/icons/check.svg",
+  pending: "/assets/icons/pending.svg",
+  cancelled: "/assets/icons/cancelled.svg",
+};
